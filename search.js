29ce6d7198a263bba6d5d6385057e1b7c5120d1b
robertweber95 <robertweber95@gmail.com>
2017-01-09 22:20:40 -0500

    Updated UI to show videos

--- search.js
+++ search.js
@@ -3,28 +3,32 @@ function start() {
   gapi.client.init({
     'apiKey': 'AIzaSyCgOL4a_gaDfNdDb_K3NefeO-KRx2REy7s',
     'discoveryDocs': ['https://people.googleapis.com/$discovery/rest'],
-  }).then(function() {
-    var request = gapi.client.request(
-    {
-      'path': 'https://www.googleapis.com/youtube/v3/search',
-      'method': 'GET',
-      'params': {
-        eventType: 'live',
-        part: 'snippet',
-        type: 'video'
-      }
-    });
-
-
-  //var request = gapi.client.youtube.search.list( );
-  request.execute(function(response) {
-    var str = JSON.stringify(response.items);
-    $('#videos').html('<pre>' + str + '</pre>');
-    console.log(response);
-  });
-}, function(reason) {
+  }).then(searchLiveVideos, function(reason) {
     console.log('Error: ' + reason.result.error.message);
   });
 };
 // 1. Load the JavaScript client library.
 gapi.load('client', start);
+
+function searchLiveVideos() {
+  var query = $("#search").val();
+  $("#search").val('');
+  $("#videos").html('');
+  var request = gapi.client.request(
+    {
+      'path': 'https://www.googleapis.com/youtube/v3/search',
+      'method': 'GET',
+      'params': {
+        q: query,
+        eventType: 'live',
+        part: 'snippet',
+        type: 'video'
+      }
+    });
+  request.execute(function(response) {
+    $.each(response.items, function(i, item){
+       $('#videos').html($('#videos').html() + "<div class=\"embed-responsive embed-responsive-16by9\"><iframe class=\"embed-responsive-item\" width=\"420\" height=\"315\" src=\"https://www.youtube.com/embed/"+ item.id.videoId +"\"></iframe></div>");
+    });
+    console.log(response);
+  });
+}
